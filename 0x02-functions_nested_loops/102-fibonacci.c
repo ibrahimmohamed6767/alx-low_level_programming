5d0c66ff429d4df92ddfb0cefe7f622a492a8890
ibrahimmohamed6767 <ibrahimmohamed5490@gmail.com>
2023-03-21 08:48:29 -0700

    finished

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,168 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+  * main - print the first 50 fibonacci numbers.
+  * Return: Nothing.
+  */
 int main(void)
 {
+	unsigned long count, i, j, k;
 
-	return (0);
-}
+	i = 0;
+	j = 1;
+	for (count = 0; count < 50#include <stdio.h>
+			/**
+			 *   * main - print the first 50 fibonacci numbers.
+			 *     * Return: Nothing.
+			 *       */
+			int main(void)
+			{
+				unsigned long count, i, j, k;
+				
+					i = 0;
+						j = 1;
+							for (count = 0; count < 50; count++)
+							{
+									k = i + j;
+											i = j;
+													j = k;
+															printf("%lu", k);
+																	if (count == 49)
+																			#include <stdio.h>
+																	/**
+																	 *   * main - print the first 50 fibonacci numbers.
+																	 *     * Return: Nothing.
+																	 *       */
+							int main(void)
+							{
+								unsigned long count, i, j, k;
+							
+								i = 0;
+								j = 1;
+								for (count = 0; count < 50; count++)
+								{
+										k = i + j;
+										i = j;
+										j = k;
+										printf("%lu", k);
+						#include <stdio.h>
+								/**
+								 *   * main - print the first 50 fibonacci numbers.
+								 *     * Return: Nothing.
+								 *       */
+								int main(void)
+								{
+									unsigned long count, i, j, k;
+								
+									i = 0;
+									j = 1;
+									for (count = 0; count < 50; count++)
+									{
+											k = i + j;
+											i = j;
+											j = k;
+											printf("%lu", k);
+											if (#include <stdio.h>
+													/**
+													 *   * main - print the first 50 fibonacci numbers.
+													 *     * Return: Nothing.
+													 *       */
+													int main(void)
+													{
+														unsigned long count, i, j, k;
+														
+															i = 0;
+																j = 1;
+																	for (count = 0; count < 50; count++)
+																	{
+																			k = i + j;
+																					i = j;
+																							j = k;
+																									printf("%lu", k);
+																											if (count == 49)
+																														putchar('\n');
+																														else
+																			#include <stdio.h>
+																	/**
+																	 *   * main - print the first 50 fibonacci numbers.
+																	 *     * Return: Nothing.
+																	 *       */
+																	int main(void)
+																	{
+																		unsigned long count, i, j, k;
+																	
+																		i = 0;
+																		j = 1;
+																		for (count = 0; count < 50; count++)
+																		{
+																				k = i + j;
+																				i = j;
+																				j = k;
+																				printf("%lu", k);
+																				if (count == 49)
+																					putchar('\n');
+																				els#include <stdio.h>
+																		/**
+																		 *   * main - print the first 50 fibonacci numbers.
+																		 *     * Return: Nothing.
+																		 *       */
+																		int main(void)
+																		{
+																			unsigned long count, i, j, k;
+																		
+																			i = 0;
+																			j = 1;
+																			for (count = 0; count < 50; count++)
+																			{
+																					k = i + j;
+																					i = j;
+																					j = k;
+																					printf("%lu", k);
+																					if (count == 49)
+																						putchar('\n');
+																					else
+				#include <stdio.h>
+																			/**
+																			 *   * main - print the first 50 fibonacci numbers.
+																			 *     * Return: Nothing.
+																			 *       */
+																			int main(void)
+																			{
+																				unsigned long count, i, j, k;
+																			
+																				i = 0;
+																				j = 1;
+																				for (count = 0; count < 50; count++)
+																				{
+																						k = i + j;
+																						i = j;
+																						j = k;
+																						printf("%lu", k);
+																						if (count == 49)
+																							putchar('\n');
+																						else
+										#include <stdio.h>
+																				/**
+																				 *   * main - print the first 50 fibonacci numbers.
+																				 *     * Return: Nothing.
+																				 *       */
+																				int main(void)
+																				{
+																					unsigned long count, i, j, k;
+																				
+																					i = 0;
+																					j = 1;
+																					for (count = 0; count < 50; count++)
+																					{
+																							k = i + j;
+																							i = j;
+																							j = k;
+																							printf("%lu", k);
+																							if (count == 49)
+																								putchar('\n');
+																							else
+																							{
+																										printf(", ");
+																							}
+																					}
+																					return (0);
+																				}}
