1440fd241f29bc1bbe49efef7ee0b0193ced9f0d
ibrahimmohamed6767 <ibrahimmohamed5490@gmail.com>
2023-04-03 15:55:41 -0700

    finished

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,12 @@
 #include "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
+while (i< n )
+{
+   *(s+i) = b;
+  i++;
+}
+return (s);
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (s);
 }
